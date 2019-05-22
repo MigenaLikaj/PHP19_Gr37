a683b3c0be67aec9b61215c63db7424b2e7951d5
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-22 10:34:07 +0200

    Update lajme.php

--- projekti/lajme.php
+++ projekti/lajme.php
@@ -1,1 +1,24 @@
-
+<!DOCTYPE html>
+<html manifest="demo_html.appcache">
+<head lang="en">
+<link rel="stylesheet" type="text/css" href="index.css">
+<meta charset="utf-8">
+<title>Lajme</title>
+<style>
+.gallery {
+    margin: 5px;
+    border: 1px solid #ccc;
+    float:left;
+    width: 350px;
+}
+.gallery:hover {
+    border: 50px #767;
+}
+.gallery img {
+    width: 100%;
+    height: auto;
+}
+.desc {
+    padding: 15px;
+    text-align: center;
+}
