02409ca2b6a6cfff61df3b8409eaaa71a7316adb
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-22 10:13:15 +0200

    Update rrethnesh.php

--- projekti/rrethnesh.php
+++ projekti/rrethnesh.php
@@ -1,1 +1,44 @@
+<!DOCTYPE html> 
+<html manifest="demo_html.appcache">
+<head lang="en">
+<link rel="stylesheet" type="text/css" href="index.css">
+<link href="https://fonts.googleapis.com/css?family=Saira|Saira+Condensed|Saira+Extra+Condensed|Saira+Semi+Condensed|Lato|Roboto" rel="stylesheet">
+<meta charset="utf-8">
+<style>
+#body {
+	margin-top:70px;
+	margin-left:70px;
+	margin-right:70px;
+	clear:both;
+	background-image:url("images/bag1.jpg");
 
+}
+#thenie {
+    border:1px solid transparent;
+	font-family: Segoe Print;
+	font-size: 12px;
+	clear:both;
+	height:500px;
+}
+#thenietleft {
+	margin-top:30px;
+	float:left;
+    height: 100px;
+    border: 1px solid transparent;
+    border-radius: 5px;
+
+}
+#thenieright {
+	float:right;
+	margin-top:30px;
+    height: 100px;
+    border: 1px solid transparent;
+    border-radius: 5px;
+	}
+pre {
+	font-family: Segoe Print;
+	font-size: 15px;
+	font-weight: bold;
+}
+</style>
+</head>
