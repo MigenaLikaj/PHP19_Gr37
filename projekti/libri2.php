097ef3535108236c0d89d196f421cef89f71b2c7
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 21:30:37 +0200

    Update libri2.php

--- projekti/libri2.php
+++ projekti/libri2.php
@@ -24,3 +24,18 @@
  i cili kishte mbaruar shkollën e lartë për nëpunës në Stamboll dhe emëruar në Kozan të Greqisë,
  ku shërbeu për 23 vjet. Në vitin 1902 Abazi kthehet në Shqipëri dhe i prirur të ndikojë sadopak në lëvizjen autonomiste shqiptare,
  i bashkëngjitet veprimit energjik të klerikëve bektashianë.
+Ai vendoset në teqenë e Prishtës, në Skrapar, e njohur në atë kohë për veprimtarinë
+ atdhetare në mënyrë të veçantë në shpërndarjen e librave shqip në krejt jugun e Shqipërisë.
+ Falë përkushtimit të tij të plotë, Abaz Skëndi merr dorë tek babai i teqesë dhe bëhet myhib
+ (shkalla e parë e praktikantëve mistik të bektashizmit). Deri sa ndërron jetë në vitin 1911,
+ ky shqiptar i madh i shërben me devotshmëri kombit të vet.
+Ali Abaz Skëndi kryen arsimin fillor në Berat, ndërsa të mesmen ne Kozan ku mëson,
+ veç greqishtes, edhe gjuhët arabe e turke. Në vitin 1913 kthehet në atdhe dhe
+ vazhdon punën e të atit në teqenë e Prishtës.
+</p>
+</div>
+<br>
+<?php include ('footer.php'); ?>
+
+</body>
+</html>
