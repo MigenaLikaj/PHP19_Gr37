333ef8b06259075958827fa17b288f1ae3bd0a11
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 20:58:22 +0200

    Update lajmi3.php

--- projekti/lajmi3.php
+++ projekti/lajmi3.php
@@ -27,3 +27,21 @@
 
 <h1 style="text-align:center; font-family:arial;" > Librat qe zhvillojne inteligjencen dhe forcojne memorien </h1>
 	<div class="lajmi1">
+
+<img style="position:relative" src="images\lajmi3.jpg" alt="Librat qe zhvillojne inteligjencen dhe forcojne memorien">
+	<br><br><br>
+
+<?php
+$myfile = fopen("lajmi3.txt", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+  echo utf8_encode(fgets($myfile)) . "<br>";
+}
+fclose($myfile);
+?>
+
+	</div>
+	<?php include ('footer.php'); ?>
+
+
+</body>
+</html>
