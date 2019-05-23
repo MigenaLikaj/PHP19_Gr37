b1e2027ee4c5bfe0177fa2fb66ffac1f740002b2
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 20:45:38 +0200

    Update lajmi4.php

--- projekti/lajmi4.php
+++ projekti/lajmi4.php
@@ -11,3 +11,22 @@
 	}
 
 </style>
+</head>
+<body>
+
+<?php include ('header.php'); ?>
+
+<h1 style="text-align:center; font-family:arial;" > Shkenca është e pamjaftueshme për ta njohur të vërtetën </h1>
+	<div class="lajmi1">
+
+		<img style="position:relative" src="images\lajmi4.jpg" alt="Shkenca është e pamjaftueshme për ta njohur të vërtetën">
+	<br><br><br>
+	<?php
+$myfile = fopen("lajmi4.txt", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+  echo utf8_encode(fgets($myfile)) . "<br>";
+}
+fclose($myfile);
+?>
+
+	</div>
