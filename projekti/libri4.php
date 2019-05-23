dd5d327e004c289056059e4f28f4ddf5c8747319
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 21:34:09 +0200

    Update libri4.php

--- projekti/libri4.php
+++ projekti/libri4.php
@@ -9,3 +9,13 @@
 	margin-top:50px;
 }
 </style>
+<title>The girl who drank the moon</title>
+</head>
+<body>
+<?php include ('header.php'); ?>
+<div id="body">
+<center>
+<img src="images\libri44.png" alt="Librikatert" width="500" height="400">
+</center>
+<figcaption align="middle"><b>LIBRI I PARE ME I SHITUR</b></figcaption>
+<h1>The girl who drank the moon</h1>
