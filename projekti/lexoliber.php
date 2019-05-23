6188a4d1a435dd7725942000e39d3c1c545bfd0e
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 21:15:56 +0200

    Update lexoliber.php

--- projekti/lexoliber.php
+++ projekti/lexoliber.php
@@ -8,3 +8,19 @@
     margin-top: 10px;
     padding-left:30px;
     }
+.carousel {
+  padding: 20px;
+  -webkit-perspective: 500px;
+          perspective: 500px;
+  overflow: hidden;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+      -ms-flex-direction: column;
+          flex-direction: column;
+  -webkit-box-align: center;
+      -ms-flex-align: center;
+          align-items: center;
+}
