e8ab4d617193953dfbc68c1e05b28ab9d14eb5f3
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-23 21:30:08 +0200

    Update libri2.php

--- projekti/libri2.php
+++ projekti/libri2.php
@@ -9,3 +9,18 @@
 	margin-top:50px;
 }
 </style>
+<title>Baba Tomorri</title>
+</head>
+<body>
+<?php include ('header.php'); ?>
+<div id="body">
+<center>
+<img src="images\libri22.jpg" alt="Libridyte" width="500" height="400">
+</center>
+<figcaption align="middle"><b>LIBRI I DYTE ME I SHITUR</b></figcaption>
+<h1>Baba Tomorri</h1>
+<p align="middle">Ali Abaz Skëndi, i quajtur ndryshe baba Ali Tomori,
+ u lind më 1893 në fshatin Shalës në Tepelenë. Ishte i biri i Abazit,
+ i cili kishte mbaruar shkollën e lartë për nëpunës në Stamboll dhe emëruar në Kozan të Greqisë,
+ ku shërbeu për 23 vjet. Në vitin 1902 Abazi kthehet në Shqipëri dhe i prirur të ndikojë sadopak në lëvizjen autonomiste shqiptare,
+ i bashkëngjitet veprimit energjik të klerikëve bektashianë.
