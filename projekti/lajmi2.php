d0ba2fb4588117dbcc74d6be5c836275f50a70be
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 20:57:34 +0200

    Update lajmi2.php

--- projekti/lajmi2.php
+++ projekti/lajmi2.php
@@ -21,3 +21,19 @@
 
 <h1 style="text-align:center; font-family:arial;" > “Një libër, një laps, një fëmijë dhe një mësues mund ta ndryshojnë botën”</h1>
 	<div class="lajmi1">
+
+<p style="text-align:justify; margin-left:100px; margin-right:100px; font-size:18px; font-family:Times New Roman;">
+		<img style="position: relative" src="images\lajmi2.jpg" alt="Një libër, një laps, një fëmijë dhe një mësues mund ta ndryshojnë botën">
+	<br><br><br>
+<?php
+$myfile = fopen("lajmi2.txt", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+  echo utf8_encode(fgets($myfile)) . "<br>";
+}
+fclose($myfile);
+?>
+	</div>
+<?php include ('footer.php'); ?>
+
+</body>
+</html>
