06ea93e7520e81d93ea83588a61fb70b6164fe5f
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 21:52:33 +0200

    Update game.php

--- projekti/game.php
+++ projekti/game.php
@@ -30,3 +30,33 @@
 </head>
 <body>
 <?php include ('header.php'); ?>
+<div style="margin-left:60px; margin-right:60px;">
+<div class="gallery" style="transform: rotate(-20deg);">
+  <a target="_blank" href="kuiz1.html">
+      <img src="images/kuiz1.jpg"
+		alt="Kuiz nga letërsia shqiptare."
+		 width="600" height="400" >
+                  <div class="desc">Kuiz nga letërsia shqiptare</div>
+  </a>
+</div>
+<div class="gallery">
+  <a target="_blank" href="kuiz2.html">
+      <img src="images/kuiz2.png"
+		alt="Kuiz nga letërsia botërore."
+		 width="600" height="400" >
+                  <div class="desc">Kuiz nga letërsia botërore</div>
+  </a>
+</div>
+<div class="gallery" style="transform: rotate(20deg);">
+  <a target="_blank" href="kuiz3.html">
+      <img src="images/qu3.jpg"
+		alt="Sa i njihni veprat e autoreve shqiptare?"
+		 width="600" height="400" >
+                  <div class="desc">Autori - Vepra</div>
+  </a>
+</div>
+</div>
+<?php include ('footer.php'); ?>
+
+</body>
+</html>
