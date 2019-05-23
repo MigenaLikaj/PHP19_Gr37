252c81b58bd171bf2dbe71d7c0aceb425bcb5821
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-23 21:33:27 +0200

    Update libri3.php

--- projekti/libri3.php
+++ projekti/libri3.php
@@ -22,3 +22,16 @@
 <p align="middle">Bagëti e Bujqësi është poemë e shkruar nga shkrimtari shqiptar Naim Frashëri
  e cila është botuar në Tiranë në vitin 1886, e cila është një nga veprat monumentale të letërsisë sonë.
  Me këtë vepër nisi edhe botimi e veprave letrare të Letërsisë së Rilindjes sonë Kombëtare.
+ Vepra Bagëti e Bujqësi nga letrarët merret si një ndër veprat më të arritura
+ të autorit dhe njëherit dhe të kohës së Rilindjes shqiptare.
+ Në të trajtohen dy tema: ajo e Bagëtisë dhe e Bujqësisë.
+ Në pjesën e parë paraqitet jeta barishtore bukuritë e tufës së dhenve.
+ Pjesa e dytë ka si temë Bujqësinë që realizohet përmes bujkut,
+ fushave pjellore, bamirësisë së tyre, pemët e mëdha dhe për natyrën që e pasuron atë vend.
+</p>
+</div>
+<br>
+<?php include ('footer.php'); ?>
+
+</body>
+</html>
