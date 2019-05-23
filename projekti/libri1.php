7ad6b90fc1914215e2e0bdbf0a4990652871982c
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 21:29:02 +0200

    Update libri1.php

--- projekti/libri1.php
+++ projekti/libri1.php
@@ -9,3 +9,24 @@
 	margin-top:50px;
 }
 </style>
+
+<title>Afshi i gjakut</title>
+	
+</head>
+<body>
+<?php include ('header.php'); ?>
+<div id="body">
+<center>
+<img src="images\libri11.jpg" alt="Libripare" width="500" height="400">
+</center>
+<figcaption align="middle"><b>LIBRI I PARE ME I SHITUR</b></figcaption>
+<h1>Afshi i gjakut</h1>
+<p align="middle"> Në një qytezë të Francës Qendrore, në fillim të viteve 1930,
+ një burrë i moshuar kujton jetën e tij. Mbasi kishte udhëtuar shumë në rininë e vet, Silvio qëndron i veçuar nga shoqëria,
+ duke vëzhguar komedinë njerëzore të fshatit, rrjedhën e qetë të fateve provinciale të tronditura befasisht
+ nga vdekja dhe nga pasionet e dashurisë.
+Para tij, Fransuai dhe Helena tregojnë takimin e tyre të parë dhe të shkurtër,
+ martesën e Helenës me një pronar plak dhe të pasur, vejaninë e saj, pritjen
+ dhe ritakimin e tyre. Kur vajza e tyre Koletë martohet me Zhan Dorënin,
+ duket sikur është skicuar rruga e një lumturie të qetë. Por, disa muaj më vonë,
+ ndodh drama.
