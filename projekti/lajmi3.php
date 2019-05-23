d94f00eebc93aba1c7a6533bb53906a35ce8b426
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 20:43:57 +0200

    Update lajmi3.php

--- projekti/lajmi3.php
+++ projekti/lajmi3.php
@@ -4,3 +4,18 @@
 <head lang="en">
 <link rel="stylesheet" type="text/css" href="index.css">
 <style>
+ 
+.bottom-left {
+    position: absolute;
+    bottom: 8px;
+    left: 16px;
+
+	}
+	.lajmi1
+	{
+	margin: auto;
+    width: 60%;
+    padding: 10px;
+	}
+
+</style>
