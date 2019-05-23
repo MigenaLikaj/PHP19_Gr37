22c88d97b233959d14f7f7db59bd9de00de31249
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 20:47:09 +0200

    Update lajmi6.php

--- projekti/lajmi6.php
+++ projekti/lajmi6.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
 
+<html manifest="demo_html.appcache">
+
+<head lang="en">
+<link rel="stylesheet" type="text/css" href="index.css">
+<style>
+
+	.lajmi1
+	{
+	margin: auto;
+    width: 80%;
+    padding: 10px;
+	}
