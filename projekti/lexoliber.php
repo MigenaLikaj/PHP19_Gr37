e98361f085a6645256920ad12c5f2598584024be
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 21:14:19 +0200

    Update lexoliber.php

--- projekti/lexoliber.php
+++ projekti/lexoliber.php
@@ -1,1 +1,10 @@
-
+<!DOCTYPE html>
+<html manifest="demo_html.appcache">
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+<title> Lexo Liber </title>
+<style>
+#libra {
+    margin-top: 10px;
+    padding-left:30px;
+    }
