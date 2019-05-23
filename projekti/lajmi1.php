8deb7bdab5814e7cb398c9228418e90980511181
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 20:35:57 +0200

    Update lajmi1.php

--- projekti/lajmi1.php
+++ projekti/lajmi1.php
@@ -1,1 +1,14 @@
+<!DOCTYPE html>
 
+<html manifest="demo_html.appcache">
+    <link href="https://fonts.googleapis.com/css?family=Saira|Saira+Condensed|Saira+Extra+Condensed|Saira+Semi+Condensed|Lato|Roboto" rel="stylesheet">
+
+<head lang="en">
+<link rel="stylesheet" type="text/css" href="index.css">
+<style>
+	.lajmi1
+	{
+	margin: auto;
+    width: 80%;
+    padding: 10px;
+	}
