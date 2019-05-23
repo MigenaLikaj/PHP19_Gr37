42e5c602be5e59ce0ef96a8115461bb0a1d7b430
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 21:32:07 +0200

    Update libri3.php

--- projekti/libri3.php
+++ projekti/libri3.php
@@ -9,3 +9,16 @@
 	margin-top:50px;
 }
 </style>
+<title>Bageti e Bujqesi</title>
+</head>
+<body>
+<?php include ('header.php'); ?>
+<div id="body">
+<center>
+<img src="images\libri33.jpg" alt="Libritrete" width="500" height="400">
+</center>
+<figcaption align="middle"><b>LIBRI I TRETE ME I SHITUR</b></figcaption>
+<h1>Bageti e Bujqesi</h1>
+<p align="middle">Bagëti e Bujqësi është poemë e shkruar nga shkrimtari shqiptar Naim Frashëri
+ e cila është botuar në Tiranë në vitin 1886, e cila është një nga veprat monumentale të letërsisë sonë.
+ Me këtë vepër nisi edhe botimi e veprave letrare të Letërsisë së Rilindjes sonë Kombëtare.
