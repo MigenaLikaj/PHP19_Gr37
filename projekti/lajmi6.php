a7fcc284ef4b8fb1329d4efa5ddaeb2dab86086e
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 20:47:29 +0200

    Update lajmi6.php

--- projekti/lajmi6.php
+++ projekti/lajmi6.php
@@ -12,3 +12,14 @@
     width: 80%;
     padding: 10px;
 	}
+</style>
+</head>
+<body>
+
+<?php include ('header.php'); ?>
+
+<h1 style="text-align:center; font-family:arial;" > Kryeqyteti ka fluksin me te madh te studenteve ,mirepo ata me se paku blejne libra </h1>
+	<div class="lajmi1">
+	<p style="text-align:justify; margin-left:100px; margin-right:100px; font-size:18px; font-family:Times New Roman;">
+		<img style="position:relative" src="images\lajmi6.jpg" alt="Kryeqyteti ka fluksin me te madh te studenteve ,mirepo ata me se paku blejne libra">
+	<br><br><br>	
