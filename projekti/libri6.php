5e4aefe39ea2156ae107f7fffe6a81aa83ebaa12
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-23 21:38:59 +0200

    Update libri6.php

--- projekti/libri6.php
+++ projekti/libri6.php
@@ -9,3 +9,19 @@
 	margin-top:50px;
 }
 </style>
+<title>1984</title>
+</head>
+<body>
+<?php include ('header.php'); ?>
+
+
+<div id="body">
+<center>
+<img src="images\libri66.jpg" alt="Librigjashte" width="500" height="400">
+</center>
+<figcaption align="middle"><b>LIBRI I TRETE ME I SHITUR</b></figcaption>
+<h1>1984</h1>
+<p align="middle">Shoqëria është e ndarë në popull, parti e gjerë dhe parti e ngushtë.
+ Jeta dominohet nga mbikëqyrja e shtetit.
+ Por viktimat e shtetit arrijnë të krijojnë simpati për mbikëqyrësin dhe me raste,
+ edhe ta dashurojnë atë.
