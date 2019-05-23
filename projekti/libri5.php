e9f632fd0fd238365fe2f35efec99067ff169e7f
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-23 21:37:16 +0200

    Update libri5.php

--- projekti/libri5.php
+++ projekti/libri5.php
@@ -9,3 +9,13 @@
 	margin-top:50px;
 }
 </style>
+<title>History of Wolves</title>
+</head>
+<body>
+<?php include ('header.php'); ?>
+<div id="body">
+<center>
+<img src="images\libri55.jpg" alt="LibriPeste" width="500" height="400">
+</center>
+<figcaption align="middle"><b>LIBRI I DYTE ME I SHITUR</b></figcaption>
+<h1>History of Wolves</h1>
