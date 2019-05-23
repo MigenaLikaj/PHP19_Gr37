adbd9f9487151b90bb9f048dbdf056f440613936
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 20:37:15 +0200

    Update lajmi1.php

--- projekti/lajmi1.php
+++ projekti/lajmi1.php
@@ -12,3 +12,13 @@
     width: 80%;
     padding: 10px;
 	}
+</style>
+</head>
+<body>
+<?php include ('header.php'); ?>
+
+    <h1 style="text-align:center; font-family:arial;">10 libra motivues – Të paktën një prej tyre duhet të jetë në listën tuaj</h1>
+	<div class="lajmi1">
+	<p style="text-align:justify; margin-left:100px; margin-right:100px; font-size:18px; font-family:Times New Roman;">
+		<img style="position: relative" src="images\lajmi1.png" alt="10 libra motivues – Të paktën një prej tyre duhet të jetë në listën tuaj">
+	<br><br><br>	
