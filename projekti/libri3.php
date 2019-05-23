8e84d55b5395601b2e0267559cc41b8b5baf4c58
BleonaZenelaj <44550465+BleonaZenelaj@users.noreply.github.com>
2019-05-23 21:11:33 +0200

    Update libri3.php

--- projekti/libri3.php
+++ projekti/libri3.php
@@ -1,1 +1,11 @@
-
+<!DOCTYPE html>
+<html manifest="demo_html.appcache">
+<head lang="en">
+<link rel="stylesheet" type="text/css" href="index.css">
+<style>
+#body{
+	margin-left:150px ;
+	margin-right:150px;
+	margin-top:50px;
+}
+</style>
