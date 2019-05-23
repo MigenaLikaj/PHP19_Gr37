b67dd538bcd33d491d847ed862fcd0d7d4972050
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-23 20:52:06 +0200

    Update lajmi5.php

--- projekti/lajmi5.php
+++ projekti/lajmi5.php
@@ -14,3 +14,11 @@
 
 </style>
 </head>
+<body>
+
+<?php include ('header.php'); ?>
+<h1 style="text-align:center; font-family:arial;" > Rreth librarise se re "BUZUKU" ne Albi Mall</h1>
+	<div class="lajmi1">
+	<p style="text-align:justify; margin-left:100px; margin-right:100px; font-size:18px; font-family:Times New Roman;">
+		<img style="position:center;" src="images\lajmi5.jpg" alt="Rreth librarise se re Buzuku ne albi mall">
+	<br><br><br>
