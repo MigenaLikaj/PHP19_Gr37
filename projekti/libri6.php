8d55c95190c03122e13da61c1892d2a4fdcfceb5
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-23 21:39:22 +0200

    Update libri6.php

--- projekti/libri6.php
+++ projekti/libri6.php
@@ -24,4 +24,17 @@
 <p align="middle">Shoqëria është e ndarë në popull, parti e gjerë dhe parti e ngushtë.
  Jeta dominohet nga mbikëqyrja e shtetit.
  Por viktimat e shtetit arrijnë të krijojnë simpati për mbikëqyrësin dhe me raste,
- edhe ta dashurojnë atë.
+ edhe ta dashurojnë atë.Libri doli në botim më 8 qershor 1949, vetëm pak muaj para se autori të mundej nga tuberkulozi.
+ Në vitin 1989 libri ishte përkthyer në mbi 65 gjuhë të ndryshme që përbënte numrin më të madh për ndonjë novel (me dallim vetëm të Harry Potter-it, mbi 67 gjuhë). Ndërkohë në Shqipëri vepra mbërriti vetëm pas rënies së komunizmit dhe kur u botua në vitin 1991. Mbrojtës i lirisë së individit, Orwell lufoi kundër çdo forme shtypjeje.
+
+Ndëshkimi i totalitarizmit fillimisht shprehet në alegorinë e veprës Ferma e kafshëve (1945),
+ një roman që pati popullaritet të gjerë në të gjithë botën,
+ me tone të një shpotie të thekshme që merr shtysë nga ngjarjet e Revolucionit rus
+ dhe tradhtisë që Stalini u bëri idealeve të tij, dhe më pas në utopinë negative të romanit 1984,
+ një kuadër tronditës i një shoqërie ku Vëllai i Madh mishëron një fuqi të aftë të kushtëzojë dhe të shtypë individualitetin e qytetarit.
+</p>
+</div>
+<br>
+<?php include ('footer.php'); ?>
+</body>
+</html>
