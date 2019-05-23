f02694a55ca49e627cd34a34b8c7dd214bedb2b6
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-23 21:51:59 +0200

    Update game.php

--- projekti/game.php
+++ projekti/game.php
@@ -1,1 +1,32 @@
+!DOCTYPE html>
+<html>
+<head manifest="demo_html.appcache">
+<head lang="en">
+<link rel="stylesheet" type="text/css" href="index.css">
+<meta charset="utf-8">
+<style>
+.gallery {
+    margin: 100px 15px 100px 130px;
+    border: 1px solid #ccc;
+    float:left;
+    width: 350px;
+}
 
+.gallery:hover {
+    border: 50px #767;
+}
+
+.gallery img {
+    width: 100%;
+    height: auto;
+}
+
+.desc {
+    padding: 15px;
+    text-align: center;
+}
+</style>
+
+</head>
+<body>
+<?php include ('header.php'); ?>
