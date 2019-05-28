2d92ec520cfa8403da02c66416dc11067825bc8f
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-28 20:08:28 +0200

    Update validation.php

--- projekti/validation.php
+++ projekti/validation.php
@@ -1,1 +1,22 @@
+<?php
+include ('database.php');
+$db = mysqli_connect(DBhost , DBuser, DBpass, DBname);
 
+$nameErr = $emailErr = $password_1Err = $password_2Err="";
+$username = $email = $password_1 =$password_2= "";
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["username"])) {
+    $nameErr = "UserName is required";
+  } else {
+    $name = test_input($_POST["username"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$username)) {
+      $nameErr = "Only letters and white space allowed"; 
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
