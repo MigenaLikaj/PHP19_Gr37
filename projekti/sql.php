c31d3572290e75e2ab8430ed05aaf211a3c2eab0
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-28 20:04:52 +0200

    Update sql.php

--- projekti/sql.php
+++ projekti/sql.php
@@ -1,1 +1,9 @@
-
+<?php
+$sql = "CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+)";
+$host='localhost';
+$username='root';
