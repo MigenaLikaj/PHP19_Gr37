fa642fa740c9e873afbaf31fac87d4c22f6a12c8
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-28 20:09:03 +0200

    Update validation.php

--- projekti/validation.php
+++ projekti/validation.php
@@ -19,4 +19,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
-  } else {
+  } 
+  else 
+  {
+     $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+
+
+if(!empty($_POST["password_1"]) && ($_POST["password_1"] == $_POST["password_2"])) {
+        $password_1 = test_input($_POST["password_1"]);
+        $password_2 = test_input($_POST["password_2"]);
+        if (strlen($_POST["password_1"]) <= '8') {
+            $password_1Err = "Your Password Must Contain At Least 8 Characters!";
+        }
+        elseif(!preg_match("#[0-9]+#",$password_1)) {
+            $password_1Err = "Your Password Must Contain At Least 1 Number!";
+        }
+        elseif(!preg_match("#[A-Z]+#",$password_1)) {
+            $password_1Err = "Your Password Must Contain At Least 1 Capital Letter!";
+        }
+        elseif(!preg_match("#[a-z]+#",$password_1)) {
+            $password_1Err = "Your Password Must Contain At Least 1 Lowercase Letter!";
+        } else {
+            $password_2Err = "Please Check You've Entered Or Confirmed Your Password!";
+        }
+    }
+ ?>
