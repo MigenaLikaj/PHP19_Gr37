7d219e6890ad6895c1acbb0f683e6bc6f9a43c4d
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-28 19:50:51 +0200

    Update database.php

--- projekti/database.php
+++ projekti/database.php
@@ -1,1 +1,20 @@
+<?php
+session_start();
+$username = "root";
+$email    = "";
+$errors = array();
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'registration');
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
+  if (empty($username)) { array_push($errors, "Username is required"); }
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
