cf4015cb4eb997d8f59654fa3e9911edd09de210
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-28 19:48:03 +0200

    Update login.php

--- projekti/login.php
+++ projekti/login.php
@@ -3,7 +3,8 @@
 <html>
 <head>
   <title>Login</title>
-  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="style.css">
 </head>
@@ -17,3 +18,22 @@
   	<div class="input-group"> 
   		<label>Username</label>
   		<input type="text" name="username" value="<?php if(isset($_COOKIE["username"])) { echo $_COOKIE["username"]; } ?>" class="input-group">
+      </div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password" value="<?php if(isset($_COOKIE["password"])) { echo $_COOKIE["password"]; } ?>" class="input-group">
+  	</div>
+    <div class="input=input-group">
+      <input type="checkbox" name="remember">
+            <label>Remember me</label>
+
+    </div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Craete an account  <a href="register.php">Sign up</a>
+  	</p>
+  </form>
+</body>
+</html>
