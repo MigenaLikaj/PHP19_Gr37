fb13e3eefb4680acca095459f071f7a889662156
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-28 19:52:19 +0200

    Update database.php

--- projekti/database.php
+++ projekti/database.php
@@ -29,7 +29,6 @@ $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$e
       array_push($errors, "email already exists");
     }
   }
-  // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
   	$query = "INSERT INTO users (username, email, password)
@@ -40,3 +39,20 @@ $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$e
   	header('location: index.php');
   }
 }
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $username = mysqli_real_escape_string($db, $_POST['username']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+  if (empty($username)) {
+  	array_push($errors, "Username is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+
+    $user=mysqli_fetch_array($results);
