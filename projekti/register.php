ffbf287d2f77ec4b7785f505375409d0e32763bb
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-28 19:49:15 +0200

    Update register.php

--- projekti/register.php
+++ projekti/register.php
@@ -1,1 +1,18 @@
+<?php include('database.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Signup</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
+<body class="container">
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
