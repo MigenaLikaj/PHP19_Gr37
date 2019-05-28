4124dc04c113706f533a3c3673b842769a947ff6
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-28 19:46:55 +0200

    Update login.php

--- projekti/login.php
+++ projekti/login.php
@@ -1,1 +1,19 @@
+<?php include('database.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Login</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body class="container">
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group"> 
+  		<label>Username</label>
+  		<input type="text" name="username" value="<?php if(isset($_COOKIE["username"])) { echo $_COOKIE["username"]; } ?>" class="input-group">
