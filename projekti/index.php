50734b1e2580a9deb3386f2395ca829617477a99
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-29 17:32:50 +0200

    Update index.php

--- projekti/index.php
+++ projekti/index.php
@@ -1,1 +1,19 @@
-
+<?php
+  session_start();
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "You must log in first";
+  	header('location: login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
