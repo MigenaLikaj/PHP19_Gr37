efabbb9b5da3fba4ce1e7694ff159a358c0e70ca
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-29 13:49:23 +0200

    Update login.php

--- projekti/login.php
+++ projekti/login.php
@@ -6,7 +6,7 @@
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body class="container">
   <div class="header">
