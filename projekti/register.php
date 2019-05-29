20c86ae9435c67e2060076c0e0b1f39b253f65a7
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-29 13:49:36 +0200

    Update register.php

--- projekti/register.php
+++ projekti/register.php
@@ -5,7 +5,7 @@
   <title>Signup</title>
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
