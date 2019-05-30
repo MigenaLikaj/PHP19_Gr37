f42360a3d8944b9a4ba86970ac051260f3be669e
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:33:46 +0200

    Update edit_post.php

--- projekti/edit_post.php
+++ projekti/edit_post.php
@@ -1,1 +1,24 @@
+<?php require 'header.php'; ?>
+<?php 
+if (isset($_GET['id'])) {
+	$id = $_GET['id'];
 
+	$post = new Post;
+
+	$edit_post = $post->readPostById($id);
+
+}
+	
+ ?>
+
+<div class="card-header">
+	<h3>Edit Your Comment <i class="fas fa-edit"></i></h3>
+</div>
+
+<div class="card-body">
+
+<form action="update_post.php" method="POST">
+
+<div class="from-group">
+		<input type="hidden" name="id" value="<?php echo $edit_post->id ?>">
+	</div>
