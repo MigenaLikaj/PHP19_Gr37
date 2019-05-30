17f7d8260ee66ca0fb4f6bd896cb0a779b56aa90
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:33:34 +0200

    Update delete_book.php

--- projekti/admin/delete_book.php
+++ projekti/admin/delete_book.php
@@ -4,3 +4,13 @@
     if(isset($_GET['delete_book'])){
     $id = $_GET['delete_book'];
     $delete_book = "delete from books where id = '$id'";
+     $del_book = mysqli_query($con,$delete_book);  
+      
+    echo "<script>alert('Book Deleted Successfully!!')</script>";
+    echo "<script>window.open('books.php','_self');</script>"; 
+        
+    };
+?>
+ 
+
+<?php include("includes/footer.php");?>
