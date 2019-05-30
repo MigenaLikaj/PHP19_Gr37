54beacd30e9ecef5290f9141a37581c01f21e273
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-30 23:31:02 +0200

    Update db.php

--- projekti/db.php
+++ projekti/db.php
@@ -1,1 +1,10 @@
+<?php 
+try {
 
+	$pdo = new PDO ('mysql:host=localhost;dbname=comment','root','');
+	
+} catch (PDOException $e) {
+
+	echo "<strong style=color:red;> Connection Error! </strong>".'<mark>'.$e->getMessage().'</mark>';
+
+}
