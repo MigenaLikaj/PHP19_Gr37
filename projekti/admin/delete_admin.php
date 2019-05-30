660d7a9f52fc472dd4efc3722520e1b275a11e13
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:32:24 +0200

    Update delete_admin.php

--- projekti/admin/delete_admin.php
+++ projekti/admin/delete_admin.php
@@ -3,3 +3,14 @@
     if(isset($_GET['del_user'])){
     $delete_user_id = $_GET['del_user'];
     $delete_user = "delete from admins where user_id = '$delete_user_id'";
+        
+        $del_user = mysqli_query($con,$delete_user);  
+      
+    echo "<script>alert('Instructor Deleted Successfully!!')</script>";
+    echo "<script>window.open('admins.php','_self');</script>"; 
+        
+    };
+?>
+ 
+
+<?php include("includes/footer.php");?>
