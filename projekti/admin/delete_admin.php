1a8f6399b1cbd244d87ceeb83367a1c41dd7508a
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:31:52 +0200

    Update delete_admin.php

--- projekti/admin/delete_admin.php
+++ projekti/admin/delete_admin.php
@@ -1,1 +1,5 @@
-
+<?php include("includes/header.php");?>
+<?php 
+    if(isset($_GET['del_user'])){
+    $delete_user_id = $_GET['del_user'];
+    $delete_user = "delete from admins where user_id = '$delete_user_id'";
