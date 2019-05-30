abd8afd0d068d35d406c2647821e6e661445f837
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:21:04 +0200

    Update homepage.php

--- projekti/homepage.php
+++ projekti/homepage.php
@@ -1,1 +1,63 @@
+<!DOCTYPE html> 
+<html manifest="demo_html.appcache">
+<head lang="en">
+    <link href="https://fonts.googleapis.com/css?family=Saira%7CSaira+Condensed%7CSaira+Extra+Condensed%7CSaira+Semi+Condensed%7CLato%7CRoboto" rel="stylesheet">
+<meta charset="utf-8">
 
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}html {
+    background: #ffffff;
+}
+.header-container {
+    display: flex;
+    flex-direction: column;
+    border-bottom: 1px solid #F2F2F2;
+    padding: 2.3em 0;
+}
+body {
+    font-family: Saira, sans-serif;
+    font-size: 1em;
+    color: #000000;
+    max-width: 1200px;
+    margin: auto;
+}
+
+.img{
+
+     display: block;
+     margin-left: 550px;
+     
+}
+.container{
+  text-align: center;
+  margin-top: 30px;
+
+}
+.btn{
+  border: 1px solid #3498db;
+  background: none;
+  padding: 30px 100px;
+  font-size: 20px;
+  font-family: "montserrat";
+  cursor: pointer;
+  margin: 10px;
+  transition:0.8s;
+  position: relative;
+  overflow: hidden;
+}
+.btn1, .btn2{
+  color: #3498db;
+}
+.btn3, .btn4{
+  color: #fff;
+}
+.btn1:hover,.btn2:hover{
+  color:#fff;
+}
+.btn3:hover,.btn4:hover{
+  color: #3498db;
+}
