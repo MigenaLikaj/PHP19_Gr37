ff2b5b8d14ff2dff27707be64d5e3b79a00b2155
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-30 23:50:45 +0200

    Update single_post.php

--- projekti/single_post.php
+++ projekti/single_post.php
@@ -1,1 +1,12 @@
+<?php require 'header1.php'; ?>
+	<?php
 
+	 if (isset($_GET['id'])) {
+	 	
+	 	$id = $_GET['id'];
+	 }
+$post = new Post;
+$fetch_singlePost = $post->readPostById($id);
+ ?>
+	<div class="card-body">
+<h3 style=font-family:cursive;class="mb-4"><?php echo $fetch_singlePost->email; ?></h3>
