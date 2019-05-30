1d5d329a233148b8123eeab563039d844ee83822
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:41:38 +0200

    Update header1.php

--- projekti/header1.php
+++ projekti/header1.php
@@ -1,1 +1,9 @@
+<?php require 'controller.php'; ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lexo Online Blog </title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
