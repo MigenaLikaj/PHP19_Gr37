3ae7aaf35127de43634ac92d805c7f3774fac618
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:58:20 +0200

    Update footer.php

--- projekti/admin/includes/footer.php
+++ projekti/admin/includes/footer.php
@@ -1,1 +1,12 @@
+<footer class="footer_area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="copyright text-center">
+                    <p>&copy;2019 | All right reserved by Lexo Online</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>      
 
