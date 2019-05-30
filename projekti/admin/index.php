e0e289639f70ce4d33a8c800464eba2ddd643311
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:38:13 +0200

    Update index.php

--- projekti/admin/index.php
+++ projekti/admin/index.php
@@ -20,3 +20,28 @@
                                             $run_post = mysqli_query($con,$post);  
                                             $num_rows = mysqli_num_rows($run_post);   
                                             ?>
+                       <div class="huge"><?php echo $num_rows;?></div> 
+                                            <div>All Books</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a href="view_categories.php">
+                                    <div class="panel-footer">
+                                        <span class="pull-left">View All</span>
+                                        <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-md-6">
+                            <div class="panel panel-red">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="col-xs-3">
+                                            <i class="fa fa-mortar-board  fa-5x"></i>
+                                        </div>
+                                        <div class="col-xs-9 text-right">
+                                            <?php
+                                            $learner = "SELECT * FROM `users`";
+                                            $run_learner = mysqli_query($con,$learner);
