6468e3c87b5d95abbc8d04670d3a963ec6e9b76d
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:32:48 +0200

    Update delete_book.php

--- projekti/admin/delete_book.php
+++ projekti/admin/delete_book.php
@@ -1,1 +1,6 @@
-
+<?php include("includes/header.php");?>
+ 
+ <?php 
+    if(isset($_GET['delete_book'])){
+    $id = $_GET['delete_book'];
+    $delete_book = "delete from books where id = '$id'";
