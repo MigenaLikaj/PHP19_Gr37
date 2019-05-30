1712d7a18c3d3e394ccf4d7674ca2ba1bdc01ef0
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:56:39 +0200

    Update admin_menus.php

--- projekti/admin/includes/admin_menus.php
+++ projekti/admin/includes/admin_menus.php
@@ -2,3 +2,9 @@
                 <div class="admin_sidebarmenu">
                     <ul>                       
                         <li><a href="insert_book.php"><i class="fa fa-location-arrow"></i>  Insert New Book</a></li>
+                      <li><a href="books.php"><i class="fa fa-diamond"></i>  View All Books</a></li>
+                        <li><a href="admins.php"><i class="fa fa-adjust"></i>  All Admins</a></li>  
+                        <li><a href="readers.php"><i class="fa fa-adjust"></i>  All Readers</a></li>  
+                    </ul>
+                </div>
+            </div>
