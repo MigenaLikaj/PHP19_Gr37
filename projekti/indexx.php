d7f35800ac69c95c352b991516a47d2716f06deb
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:44:41 +0200

    Update indexx.php

--- projekti/indexx.php
+++ projekti/indexx.php
@@ -8,3 +8,15 @@ $fetch_post = $post->readAllPost();
 foreach ($fetch_post as $value):
 	
  ?>
+<h3><a style="text-decoration: none; font-family: cursive;" href="single_post.php?id=<?php echo $value->id; ?>"><?php echo $value->email; ?></a></h3>
+
+	<a class="btn btn-primary mt-3" href="edit_post.php?id=<?php echo $value->id; ?>">Edit</a>
+	<a class="btn btn-danger mt-3" href="delete_post.php?id=<?php echo $value->id; ?>">Delete</a>
+	<p style=font-family:roboto; class="mt-3"><?php echo substr($value->content, 0,300); ?>&nbsp;<a style="text-decoration: none;" href="single_post.php?id=<?php echo $value->id; ?>">read more...</a></p>
+
+<?php endforeach; ?>
+
+</div>
+
+
+<?php require 'footer1.php'; ?>
