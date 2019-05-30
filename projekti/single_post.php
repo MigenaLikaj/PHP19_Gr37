89824647f78185c58039539ff247b22b23458d84
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:51:29 +0200

    Update single_post.php

--- projekti/single_post.php
+++ projekti/single_post.php
@@ -10,3 +10,17 @@ $fetch_singlePost = $post->readPostById($id);
  ?>
 	<div class="card-body">
 <h3 style=font-family:cursive;class="mb-4"><?php echo $fetch_singlePost->email; ?></h3>
+<a class="btn btn-primary mb-4" href="edit_post.php?id=<?php echo $fetch_singlePost->id;?>">Edit</a>
+<a class="btn btn-danger mb-4" href="delete_post.php?id=<?php echo $fetch_singlePost->id; ?>">Delete</a>
+
+<p style=font-family:roboto;><?php echo $fetch_singlePost->content; ?></p>
+
+</div>
+
+
+
+
+
+
+ 
+<?php require 'footer1.php'; ?>
