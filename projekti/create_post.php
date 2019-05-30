e94a02f630f3122a882c1ddf1ed5a8f74a13c24f
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:27:24 +0200

    Update create_post.php

--- projekti/create_post.php
+++ projekti/create_post.php
@@ -15,3 +15,22 @@ if (isset($_GET['empty'])) {
 	<?php if(!empty($msg)): ?>
 		<div class="alert alert-danger"><?php echo $msg; ?></div>
 	<?php endif; ?>
+	<form action="create_post_actions.php" method="POST">
+	
+
+	<div class="from-group">
+		<label for="email">Email</label>
+		<input type="text" name="email" class="form-control" placeholder="Enter Email Here" >
+	</div>
+
+	<div class="form-group mt-2">
+		<p>Content</p>
+		<textarea name="content" cols="130" rows="10" placeholder="write your comment here..."></textarea>
+	</div>
+
+	<button name="publish" class="btn btn-primary ">Publish</button>
+
+</form>
+
+
+</div>
