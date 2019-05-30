ab484d3bcac34cfe1261b16631b5cdfb999f78bb
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-30 23:42:37 +0200

    Update header1.php

--- projekti/header1.php
+++ projekti/header1.php
@@ -7,3 +7,18 @@
 	<title>Lexo Online Blog </title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container mt-5">
+
+	<div class="card">
+		<div class="card-header text-center">
+			<h1  style=font-family:Courier; class="mb-4"><b>Leave a Comment</b></h1>
+
+            <a class="btn btn-outline-primary" href="LexoOnline.php">Kthehu</a>
+			<a class="btn btn-outline-primary" href="indexx.php">Home</a>
+			<a class="btn btn-outline-primary" href="create_post.php">Add Comment</a>
+
+
+		</div>
