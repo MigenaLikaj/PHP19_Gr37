00dbe1a0ebbb7ed33fcd2bf6b41afdfef777c1a7
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-30 23:26:43 +0200

    Update create_post.php

--- projekti/create_post.php
+++ projekti/create_post.php
@@ -1,1 +1,17 @@
+<?php require 'header1.php'; ?>
+<?php 
+$msg = '';
+if (isset($_GET['empty'])) {
+	
+	$msg = 'Field must not be empty!';
+}
+ ?>
+<div class="card-header">
+		<h3>Write Your Comment <i class="fas fa-pen-square"></i></h3>
+	</div>
 
+<div class="card-body">
+
+	<?php if(!empty($msg)): ?>
+		<div class="alert alert-danger"><?php echo $msg; ?></div>
+	<?php endif; ?>
