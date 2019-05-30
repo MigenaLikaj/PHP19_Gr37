ad8fae9b27184f6443f786698dc975c86796ba3d
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:56:23 +0200

    Update admin_menus.php

--- projekti/admin/includes/admin_menus.php
+++ projekti/admin/includes/admin_menus.php
@@ -1,1 +1,4 @@
-
+<div class="col-md-3 col-sm-3 col-xs-12">
+                <div class="admin_sidebarmenu">
+                    <ul>                       
+                        <li><a href="insert_book.php"><i class="fa fa-location-arrow"></i>  Insert New Book</a></li>
