c90c5e8a9db2408d656064b8f96cfb3dce2b3a43
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:48:49 +0200

    Update readers.php

--- projekti/admin/readers.php
+++ projekti/admin/readers.php
@@ -1,1 +1,19 @@
-
+<?php include("includes/header.php");?>
+<section class="admin_area">
+    <div class="container">
+        <div class="row">
+            <?php include("includes/admin_menus.php");?>
+            <div class="col-md-9 col-sm-9 col-xs-12">
+                <div class="add_admin_content">
+                   <h2>View All Readers</h2>
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Readers Id</th>
+                            <th>Username</th>  
+                            <th>Readers Email</th> 
+                          </tr>
+                        </thead>
+                        <tbody>
+                        <?php 
+                        $user = "select * from users";
