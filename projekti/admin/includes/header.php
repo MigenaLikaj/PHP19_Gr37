f5b765297220381f96d105f85fd024e09eee53da
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:57:25 +0200

    Update header.php

--- projekti/admin/includes/header.php
+++ projekti/admin/includes/header.php
@@ -1,1 +1,30 @@
+<?php 
+include('includes/connection.php');
+session_start();    
+if(!isset($_SESSION['user_name'])){
+    header("location: login.php");
+}
+?>
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>E-Learning</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+        <link href="css/jquery.bxslider.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+        <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+        <script>tinymce.init({ selector:'textarea' });</script> 
+        <style>
+            h1 {
