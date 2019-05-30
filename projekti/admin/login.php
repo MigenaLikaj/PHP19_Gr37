3233a8467a769b5ba7acf9b2ed1dbc99554dbb8a
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:46:31 +0200

    Update login.php

--- projekti/admin/login.php
+++ projekti/admin/login.php
@@ -19,3 +19,22 @@ include('includes/connection.php');
         <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
         <script src="js/modernizr-2.8.3.min.js"></script>        
     </head>
+  <body>
+         <section class="login_area">
+            <div class="container">
+               
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="logo">
+                            <img src="logo3.png" alt="E-learning">
+                        </div>
+                        <div class="login_form"> 
+                           <h2 style="text-align:center;margin:20px 0;">Admin's Login</h2>
+                            <form name="form" id="form" class="form-horizontal" enctype="multipart/form-data" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                                    <input id="user" type="text" class="form-control" name="user" placeholder="User" required>                                        
+                                </div>
+
+                                <div class="input-group">
