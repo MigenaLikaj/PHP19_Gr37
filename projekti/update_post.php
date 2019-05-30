576939e100acdd924fa48c337892358b85b15cd7
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:53:35 +0200

    Update update_post.php

--- projekti/update_post.php
+++ projekti/update_post.php
@@ -4,3 +4,14 @@ if (isset($_POST['update'])) {
 		$id = $_POST['id'];
 	$email = $_POST['email'];
 	$content = $_POST['content'];
+$post = new Post;
+
+$update_post = $post->updatePost($id,$email,$content);
+
+header('location:single_post.php?id='.$id);
+
+}
+
+
+
+?>
