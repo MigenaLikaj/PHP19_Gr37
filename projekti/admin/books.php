129292bcc277e8af518349affb1b06d8121bd335
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:29:46 +0200

    Update books.php

--- projekti/admin/books.php
+++ projekti/admin/books.php
@@ -1,1 +1,23 @@
-
+<?php include("includes/header.php");?>
+<section class="admin_area">
+    <div class="container">
+        <div class="row">
+            <?php include("includes/admin_menus.php");?>
+            <div class="col-md-9 col-sm-9 col-xs-12">
+                <div class="add_admin_content">
+                   <h2>View All Books</h2>
+                      <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th>Book Id</th>
+                            <th>Book Name</th>
+                            <th>Book Description</th>
+                            <th>Book Category</th>
+                            <th>Book Price</th> 
+                            <th>Delete Book</th>
+                                                       
+                          </tr>
+                        </thead>
+                        <tbody>
+                        <?php 
+                        $cat = "select * from books";
