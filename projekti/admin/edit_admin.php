878715b5f4c6655bc6cff91ab22821e91768b8fe
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:34:47 +0200

    Update edit_admin.php

--- projekti/admin/edit_admin.php
+++ projekti/admin/edit_admin.php
@@ -19,3 +19,32 @@
                     $user_email = $user_row ['user_email'];   
                     $admin = $user_row ['admin'];   
                     ?>
+                         <form action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+                        <p>
+                            <label for="user_fname">User First Name</label>
+                            <input type="text" placeholder="User First Name" name="user_fname" value="<?php echo $user_fname;?>">
+                        </p> 
+                        <p>
+                            <label for="user_lname">User Last Name</label>
+                            <input type="text" placeholder="User Last Name" name="user_lname" value="<?php echo $user_lname;?>">
+                        </p>
+                        <p>
+                            <label for="user_name">User Name</label>
+                            <input type="text" placeholder="User Name" name="user_name" value="<?php echo $user_name;?>">
+                        </p>
+                        <p>
+                            <label for="user_email">User Email</label>
+                            <input type="email" placeholder="User Email" name="user_email" value="<?php echo $user_email;?>">
+                        </p>
+                         <input type="hidden" name="hidden_id" value="<?php echo $user_id;?>">
+                        <p>
+                            <label for="user_is_admin">User Is Admin</label>
+                            <input type="text" placeholder="User Is Admin?" name="user_is_admin" value="<?php echo $admin;?>">
+                            <span>If you want to assign this user to admin then put '1' here.</span>
+                        </p>   
+                        <p> 
+                            <input type="submit" value="Update" name="update_user">
+                        </p>
+                    </form>
+                    <?php endwhile;?>
+                    <?php };?>
