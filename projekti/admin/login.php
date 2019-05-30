d5d2bb3027a6c6eeaf88db1de7d3e30d7f5a7831
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 01:46:02 +0200

    Update login.php

--- projekti/admin/login.php
+++ projekti/admin/login.php
@@ -1,1 +1,21 @@
-
+<?php 
+include('includes/connection.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Login</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link href="css/jquery.bxslider.css" rel="stylesheet">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/responsive.css">
+        <link href='https://fonts.googleapis.com/css?family=Lora:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+        <script src="js/modernizr-2.8.3.min.js"></script>        
+    </head>
