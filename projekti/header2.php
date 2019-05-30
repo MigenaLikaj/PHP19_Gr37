159ac6d3380b5b97868dcc9b3081ad5143a65ca6
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:40:18 +0200

    Update header2.php

--- projekti/header2.php
+++ projekti/header2.php
@@ -1,1 +1,9 @@
+<link href="https://fonts.googleapis.com/css?family=Saira%7CSaira+Condensed%7CSaira+Extra+Condensed%7CSaira+Semi+Condensed%7CLato%7CRoboto" rel="stylesheet">
 
+<header class="header-container">
+        <div class="logo">
+            <a href="LexoOnline.php" title="Lexo Online"><img src="images/logo1.jpg" ></a>
+        </div>
+
+ 
+</header>
