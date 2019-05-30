edb5d028e869a2c7a6db95fa1f73e78acc391d52
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-31 01:43:10 +0200

    Update insert_book.php

--- projekti/admin/insert_book.php
+++ projekti/admin/insert_book.php
@@ -1,1 +1,10 @@
-
+<?php include("includes/header.php");?>  
+<section class="admin_area">
+    <div class="container">
+        <div class="row">
+            <?php include("includes/admin_menus.php");?>
+            <div class="col-md-9 col-sm-9 col-xs-12">
+                <div class="add_admin_content">
+                   <h2>Insert New Book</h2>
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data" method="post">
+                        <p>
