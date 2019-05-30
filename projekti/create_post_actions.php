37b6e801b0274769c898a8104f78b4b5cb65d326
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-05-30 23:29:01 +0200

    Update create_post_actions.php

--- projekti/create_post_actions.php
+++ projekti/create_post_actions.php
@@ -1,1 +1,9 @@
+<?php require 'controller.php'; ?>
+<?php 
 
+if (isset($_POST['publish'])) {
+
+	$email = $_POST['email'];
+	$content = $_POST['content'];
+
+	if (!empty($email) && !empty($content)) {
