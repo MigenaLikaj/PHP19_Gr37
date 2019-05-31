1a630666ded4f629a90c916cbd1f28ec28b0a6d1
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-05-31 13:35:00 +0200

    Update signup.php

--- projekti/admin/signup.php
+++ projekti/admin/signup.php
@@ -62,7 +62,7 @@ include('includes/connection.php');
                                 </div>
                                 <p style="color:#fff;font-size:16px;">Already an Admin? Login <strong><a href="login.php">Here</a></strong> </p>
                             </form>
-                            <p style="margin:20px 0;color:#222;text-align:center;font-size:18px;">Go back to the <strong><a href="index.php">HomePage</a></strong> </p>      
+                            <p style="margin:20px 0;color:#222;text-align:center;font-size:18px;">Go back to the <strong><a href="../homepage.php">HomePage</a></strong> </p>      
                         </div>
                     </div>
                 </div>
