f9840cc9a093cfc0ff8f24f7b58ff9037f7608ab
FluturaHaxhaj <44637987+FluturaHaxhaj@users.noreply.github.com>
2019-06-02 19:22:17 +0200

    Add files via upload

---
+++ projekti/admin/sql.sql
@@ -0,0 +1,52 @@
+-- Database: `elearning`
+--
+-- Table structure for table `admins`
+--
+CREATE TABLE IF NOT EXISTS `admins` (
+  `user_id` int(100) NOT NULL AUTO_INCREMENT,
+  `user_fname` text NOT NULL,
+  `user_lname` text NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
+  `user_pass` varchar(50) NOT NULL,
+  `about` text NOT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+
+
+INSERT INTO `admins` (`user_id`, `user_fname`, `user_lname`, `user_name`, `user_email`, `user_pass`, `about`, `admin`) VALUES
+(1, 'Flutura', 'Haxhaj ', 'FluturaH', 'flutura.haxhaj@studentet.uni-pr.edu', '39dcaf7a053dc372fbc391d4e6b5d693', '', 1);
+
+
+--
+-- Table structure for table `users`
+--
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT ,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1  ;
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
+(1, 'MigenaL', 'migena.likaj@studentet.uni-pr.edu',   '5ec829debe54b19a5f78d9a65b900a39');
+
+--
+-- Table structure for table `books`
+--
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `category` varchar(50) NOT NULL,
+   `price` integer NOT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
+
+INSERT INTO `books` (`id`, `name`, `description`, `category`, `price`) VALUES
+(1,'Sikur te isha djale, Haki Stermilli','Jeta ime për të tjerët, ndoshta nuk, nuk e vlen
+ asnji dysh, por për mue ka rëndësi, se asht imja, se mund të përshkohet nëpër faza të
+ ndryshme interesante dhe dikur, tue i këndue në këto fletë gëzimet ose hidhërimet e mia, 
+ do të kënaqem ose do të pezmatohem.','Roman','15');
