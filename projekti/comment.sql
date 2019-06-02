45fd839f2a4ba832284f9f17337c46148dd91582
MigenaLikaj <44637882+MigenaLikaj@users.noreply.github.com>
2019-06-02 20:53:05 +0200

    Update comment.sql

--- projekti/comment.sql
+++ projekti/comment.sql
@@ -1,3 +1,16 @@
+--
+-- Host: 127.0.0.1    Database: comment
+-- ------------------------------------------------------
+-- Server version       5.1.51-debug-log
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
 -- Database: `comment`
 --
 -- Table structure for table `user_comment`
@@ -12,3 +25,7 @@ PRIMARY KEY (`id`)
 
 INSERT INTO `user_comment` (`id`,  `email`, `content`) VALUES
 (1, 'flutura.haxhaj@studentet.uni-pr.edu',  'Amazing page!');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
